c2cb052049a8d6e775626d29401e754cf59d8ecf
Lorenzo <lpresta@pxltk.com>
2021-01-14 15:47:58 +0100

    test

--- index.php
+++ index.php
@@ -19,4 +19,11 @@ echo "Success: A proper connection to MySQL was made! The my_db database is grea
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
 mysqli_close($link);
+
+if($_SERVER['REQUEST_METHOD']==='GET'){
+    echo 'Method GET';
+}
+else{
+    echo 'Method diverso';
+}
 ?>
